a341a88c87fa130d4552daeefb7989bad8df55da
malinga <ekanayaka076@gmail.com>
2023-09-18 08:10:07 +0530

    completed all component level UI changes

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+
+import classes from './Cart.module.css';
+import Modal from './Modal';
+
+const Cart = (props) => {
+
+    const cartItems = <ul className={classes['cart-items']}>{[{ id: 'c1', name: 'sushi', amount: 2, price: 12.99 }].map((item) => <li>{item.name}</li>)}</ul>;
+    
+   
+
+    return <Modal onClose={props.onHideCart}>
+        {cartItems}
+        <div className={classes.total}>
+            <span>Total AMount</span>
+            <span>35.62</span>
+
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt ']} onClick={props.onHideCart}>close</button>
+            <button className={classes.button}>Order</button>
+
+        </div>
+    </Modal>
+
+}
+
+export default Cart;
